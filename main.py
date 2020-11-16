8ead9f4c382e62f26f12d1dfda8f0fdbef1c4d0b
Roman Shveda <rshveda@gmail.com>
2020-11-16 12:46:36 +0200

    Added New, Edit, Delete, Search endpoints to the api

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ from bson import json_util, ObjectId
 
 async def setup_db():
     db = AsyncIOMotorClient().car_list
-    # await db.cars.insert_one({'manufacturer': 'Toyota',
-    #                            'model': "Siena",
-    #                            'year': 2001,
-    #                            'color': "Blue",
-    #                            'VIN': "fed243edwe"})
     return db
 
 
@@ -32,12 +27,46 @@ async def cars_list(request):
     # return web.Response(status=200, body=json.dumps(json_docs), content_type='application/json')
 
 
+async def search(request):
+    db = request.app['db']
+    query = await request.json()
+    cursor = await db.cars.find({'model': query["model"]}).to_list(length=100)
+    json_doc = json_util.dumps(cursor, default=json_util.default)
+    return web.json_response(json_doc)
+
+
 async def get_car(request):
     db = request.app['db']
-    id = request.match_info.get('id')
-    cursor = await db.cars.find_one({'_id': ObjectId(id)})
+    _id = request.match_info.get('id')
+    cursor = await db.cars.find_one({'_id': ObjectId(_id)})
     json_doc = json_util.dumps(cursor)
     return web.json_response(json_doc)
+
+
+async def update_car(request):
+    db = request.app['db']
+    _id = request.match_info.get('id')
+    data = await request.json()
+    new_document = await db.cars.replace_one({'_id': ObjectId(_id)}, data)
+    return web.Response(status=200)
+
+
+async def new_car(request):
+    # await db.cars.insert_one({'manufacturer': 'Toyota',
+    #                            'model': "Siena",
+    #                            'year': 2001,
+    #                            'color': "Blue",
+    #                            'VIN': "fed243edwe"})
+    document = {'key': 'value'}
+    result = await db.cars.insert_one(document)
+    print('result %s' % repr(result.inserted_id))
+    return web.Response(status=200)
+
+
+async def delete_car(request):
+    db = request.app['db']
+    _id = request.match_info.get('id')
+    await db.cars.delete_many({'_id': ObjectId(_id)})
 
 
 async def hello(request):
@@ -49,7 +78,11 @@ db = loop.run_until_complete(setup_db())
 app = web.Application()
 app.add_routes([web.get('/', hello)])
 app.add_routes([web.get('/cars', cars_list)])
+app.add_routes([web.post('/cars', search)])
 app.add_routes([web.get('/cars/{id}', get_car)])
+app.add_routes([web.put('/cars/{id}', update_car)])
+app.add_routes([web.delete('/cars/{id}', delete_car)])
+app.add_routes([web.post('/cars/new', new_car)])
 app['db'] = db
 # Route requests to the page_handler() coroutine.
 # app.router.add_get('/pages/{page_name}', page_handler)
